8baf252c27ab61c4d8e471c3b3f9dbb67579e7ba
Munna <96653272+Munna764592@users.noreply.github.com>
2023-01-22 18:24:33 +0530

    Add files via upload

---
+++ PHP(apidata)/apidata.php
@@ -0,0 +1,18 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods: GET,POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+try {
+
+    $url = "https://api.spacexdata.com/v3/rockets";
+    $res = file_get_contents($url);
+
+    if ($res != false) {
+        // echo "<pre>";
+        echo $res;
+        // echo "</pre>";
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
